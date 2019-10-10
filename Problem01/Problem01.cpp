69b2592873e2933e06571ccb441a5d55cbf2a47c
21800130 <21800130@handong.edu>
2019-10-10 13:48:53 +0900

    Problem01

--- Problem01/Problem01.cpp
+++ Problem01/Problem01.cpp
@@ -4,17 +4,21 @@
 
 char* generate_3_digit()
 {
+	char* res = new char[10];
 
+	for(int i = 0; i <10; i++)
+		res[i] = i + '0'; //아스키코드 0~9
 
+	for(int i = 0; i < 3; i++)
+	{
+		char buf;
+		int index = rand() % (10 -i);
+		buf = res[index];
+		res[i] = res[index];
+		res[index] = buf;
+	}
 
-
-
-
-
-
-
-
-
+	return res;
 
 
 }
@@ -39,29 +43,33 @@ int main()
 		std::cout << "Guess three number: (ex. 1 2 3)" << std::endl;
 		std::cin >> guess[0] >> guess[1] >> guess[2];
 		
-	
+		for(int i = 0; i < 3; i++)
+		{
+			if(answer[i] == guess[i])
+			{
+				strike++;
+				continue;
+			}
+			else
+			{
+				bool bFlag = false;
+				for(int j = 0; j < 3; j++)
+				{
+					if(answer[j] == guess[i])
+					{
+						ball++;
+						bFlag = true;
+						break;
+					}
+				}
 
+				if (!bFlag)
+				{
+					out++;
+				}
+			}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		}
 	
 		std::cout << "S:" << strike << " B:" << ball << " O:"<< out << std::endl;
 	}
